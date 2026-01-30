44a05e20223b71d24d874850875a2028680a174b
{FuminariAriyoshi} <{arifumi_17922@icloud.com}>
2026-01-30 18:19:34 +0900

    Update favicon link and eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -9,8 +9,7 @@ module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy('js');
   eleventyConfig.addPassthroughCopy('img');
   eleventyConfig.addPassthroughCopy('styles.css');
-  eleventyConfig.addPassthroughCopy('load.css');
-  eleventyConfig.addPassthroughCopy('load.html');
+  eleventyConfig.addPassthroughCopy('404.html');
 
   const options = {
     html: true,
